04ba5da1b3f9c14b58b535914f08f4ec85085ea8
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-11 14:21:01 +0000

    Submit exercise number 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = "";
+    for(let i = 0 ; i < string.length; i++){
+        res += string[string.length -1 - i];
+    }
+    return res;
 };
 
 // Do not edit below this line
