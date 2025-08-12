1a7a5bd794f4897472c99d368869f114b02e6c32
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-12 20:50:18 +0300

    Submit exercise number 08.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+    return arr.filter(function (x) {
+        return !nums.includes(x);
+    });
 };
 
 // Do not edit below this line
