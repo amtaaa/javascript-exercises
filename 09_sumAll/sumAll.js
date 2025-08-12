eb8f12a8af3f9b9ee298e5db5cf74502fae19691
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-12 21:13:07 +0300

    Submit number 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }
+    if(start > end){
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+    let res = 0;
+    for(let i = start ; i <= end ; i++){
+        res+=i;
+    }
+    return res;
 };
 
 // Do not edit below this line
