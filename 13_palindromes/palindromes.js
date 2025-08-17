b8520612f823792dc4b5fcf4a01c568de3393587
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-17 09:35:34 +0000

    Submit number 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replaceAll(".","");
+    string = string.replaceAll("!","");
+    string = string.replaceAll(",","");
+    string = string.replaceAll(" ","");
+    string = string.toLowerCase();
+    for(let i = 0 ;i < string.length/2;i++){
+        if(string[i]!=string[string.length-1-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+palindromes('racecar');
