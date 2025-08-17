ddcc9399657b79552a3eb29e5f266b6396e84e42
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-17 09:16:34 +0000

    Submit exercise number 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr === undefined || arr.length ==0){
+    return 0;
+  }
+	return arr.reduce((a,b)=>a+b);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b)=>a*b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let res = 1;
+  for(let i = 0 ; i < b ; i++){
+    res*=a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1;
+  for(let i = 1 ; i <= a ; i++){
+    res*=i;
+  }
+  return res;
 };
 
 // Do not edit below this line
